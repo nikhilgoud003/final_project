4a08bfb968fc5bf9382ec1221d8789ac900fec4f
koushik2002vas5 <koushiksai.vas5@gmail.com>
2025-05-03 19:18:28 -0400

    added splash screen with centered loading indicator

---
+++ lib/splash_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MySplash extends StatefulWidget {
+  const MySplash({super.key});
+
+  @override
+  State<MySplash> createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(
+          color: Colors.green,
+        ),
+      ),
+    );
+  }
+}
